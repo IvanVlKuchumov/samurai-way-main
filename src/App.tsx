81b38d0cb1161e089459ab3471f20954037f659e
IvanVlKuchumov <kuchumoviv@gmail.com>
2022-12-08 22:35:57 +0300

    Add layout and breakdown by components it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Header} from './components/Header'
+import {Navigation} from "./components/Navigation";
+import {Profile} from "./components/Profile";
 
 
 const App = () => {
     return (
-        <div className="App">
-
+        <div className="app-wrapper">
+            <Header/>
+            <Navigation/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <>
+            <main className="content">
+                <div>
+                    <img id="img-width-1"
+                         src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
+                </div>
+                <div>
+                    <img id="img-width-2"
+                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXimJflwHru_9HwlTzkzmlYWhw5DMcWaApkg7EpQJrQL17oa2gWGWc8R79Bs936O5GAsA&usqp=CAU"/>
+                </div>
+                <div>
+                    My post
+                </div>
+                <div>
+                    New post
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </main>
+        </>
+    );
+}
