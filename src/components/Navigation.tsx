10929bb7618dae11c3a519586c7c1da5b01a0482
IvanVlKuchumov <kuchumoviv@gmail.com>
2022-12-09 22:27:12 +0300

    Add css modules

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,19 +1,20 @@
 import React from "react";
+import s from "./Navigation.module.css";
 
 export const Navigation = () => {
     return (
         <>
-            <nav className="nav">
-                <div>
+            <nav className={s.nav}>
+                <div className={s.item}>
                     <a>Profile</a>
                 </div>
-                <div>
+                <div className={s.item}>
                     <a>Messages</a>
                 </div>
-                <div>
+                <div className={s.item}>
                     <a>News</a>
                 </div>
-                <div>
+                <div className={s.item}>
                     <a>Music</a>
                 </div>
             </nav>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,27 +1,27 @@
 import React from "react";
-
+import s from "./Profile.module.css"
 export const Profile = () => {
     return (
         <>
-            <main className="content">
+            <main className={s.content}>
                 <div>
-                    <img id="img-width-1"
+                    <img id={s.img_width_1}
                          src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
                 </div>
                 <div>
-                    <img id="img-width-2"
+                    <img id={s.img_width_2}
                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXimJflwHru_9HwlTzkzmlYWhw5DMcWaApkg7EpQJrQL17oa2gWGWc8R79Bs936O5GAsA&usqp=CAU"/>
                 </div>
-                <div>
+                <div className={s.item}>
                     My post
                 </div>
-                <div>
+                <div className={s.item}>
                     New post
                 </div>
-                <div>
+                <div className={s.item}>
                     post1
                 </div>
-                <div>
+                <div className={s.item}>
                     post2
                 </div>
             </main>
