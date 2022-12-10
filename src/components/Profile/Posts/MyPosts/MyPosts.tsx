7f32373a642048d0d6d09b180f12ffaf34575b2b
IvanVlKuchumov <kuchumoviv@gmail.com>
2022-12-10 12:41:32 +0300

    Add propsType

--- src/components/Profile/Posts/MyPosts/MyPosts.tsx
+++ src/components/Profile/Posts/MyPosts/MyPosts.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {Post} from "./Post/Post";
+
 export const MyPosts = () => {
     return (
         <>
                 <textarea></textarea>
                 <button>Add post</button>
-                <Post />
-                <Post />
-                <Post />
+                <Post messages="Hello, my friend!" likeCount={1} />
+                <Post messages="The boy went to success, no luck." likeCount={0}/>
+                <Post messages="Chocolate is not to blame" likeCount={100500}/>
         </>
     );
 }
--- src/components/Profile/Posts/MyPosts/Post/Post.tsx
+++ src/components/Profile/Posts/MyPosts/Post/Post.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import s from "./Post.module.css"
+export type PropsPostType = {
+    messages: string
+    likeCount: number
+}
 
-export const Post = () => {
+export const Post: React.FC<PropsPostType> = (props) => {
     return (
         <>
             <div className={s.item}>
                 <img className={s.img}
                     src='https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector.png}'
                     alt='avatar'/>
-                My post
+                {props.messages}
             </div>
+            <span>Like {props.likeCount}</span>
         </>
     );
 }
