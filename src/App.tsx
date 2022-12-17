839d427f5454f290772b8611924fe240562fa8fb
IvanVlKuchumov <kuchumoviv@gmail.com>
2022-12-17 21:13:53 +0300

    Add page Music Settings and News. Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import {Header} from './components/Header/Header'
 import {Navigation} from "./components/Navigation/Navigation";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import {Music} from "./components/Music/Music";
+import {News} from "./components/News/News";
+import {Settings} from "./components/Settings/Settings";
 
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navigation/>
-            {/*<Profile className="app-wrapper-content"/>*/}
-            <div className="app-wrapper-content">
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navigation/>
+                <div className="app-wrapper-content">
+                    <Route path='dialogs' component={Dialogs}/>
+                    <Route path='profile' component={Profile}/>
+                    <Route path='music' component={Music}/>
+                    <Route path='news' component={News}/>
+                    <Route path='settings' component={Settings}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
