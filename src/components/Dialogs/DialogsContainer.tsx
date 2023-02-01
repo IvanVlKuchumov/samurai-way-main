fb818220a85eadc5598696306912d93d645f3afd
IvanVlKuchumov <kuchumoviv@gmail.com>
2023-02-01 22:16:04 +0300

    StoreContext

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {StorePropsType} from "../../redux/redux-store";
 import {sendMessageAC, updateNewMessageBodyAC} from "../../redux/dialogs-reducer";
 import {Dialogs} from "./Dialogs";
 import {StoreContext} from "../../StoreContext";
@@ -8,23 +7,26 @@ import {StoreContext} from "../../StoreContext";
 export const DialogsContainer = () => {
 
 
-    <StoreContext.Consumer>
-        {const state = store.getState()
+    return (<StoreContext.Consumer>
+        {
+            store => {
+                const state = store.getState()
+                const updateNewMessageBody = (body: string) => {
+                    store.dispatch(updateNewMessageBodyAC(body))
+                }
+                const sendMessage = () => {
+                    store.dispatch(sendMessageAC())
+                }
 
-            const updateNewMessageBody = (body: string) => {
-            store.dispatch(updateNewMessageBodyAC(body))
+                return (
+                    <Dialogs
+                        dialogsPages={state.dialogsPages}
+                        updateNewMessageBody={updateNewMessageBody}
+                        sendMessage={sendMessage}
+                    />
+                )
+            }
         }
+    </StoreContext.Consumer>)
 
-
-            const sendMessage = () => {
-            store.dispatch(sendMessageAC())
-        }
-            (return (
-            <Dialogs
-            dialogsPages={state.dialogsPages}
-            updateNewMessageBody={updateNewMessageBody}
-            sendMessage={sendMessage}
-            />
-            ))}
-    </StoreContext.Consumer>
 }
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import s from "./Profile.module.css"
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
-import {StorePropsType} from "../../redux/redux-store";
 import {MyPostsContainer} from "./Posts/MyPosts/MyPostsContainer";
+import {StoreContext} from "../../StoreContext";
 
 
-type ProfileType = {
-    store: StorePropsType
-}
+export const Profile = () => {
 
-
-export const Profile:React.FC<ProfileType> = (props) => {
-
-    return (
-        <>
-            <main className={s.content}>
-                <ProfileInfo/>
-                <MyPostsContainer store={props.store}/>
-            </main>
-        </>
-    );
+    return (<StoreContext.Consumer>
+        {
+            store => {
+                return (
+                    <main className={s.content}>
+                        <ProfileInfo/>
+                        <MyPostsContainer store={store}/>
+                    </main>
+                )
+            }
+        }
+    </StoreContext.Consumer>);
 }
