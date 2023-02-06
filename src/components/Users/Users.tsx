a07eda255ac72c286a5bdb523d99b648de84be05
IvanVlKuchumov <kuchumoviv@gmail.com>
2023-02-06 21:12:30 +0300

    Add  componentDidMount for components User

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,8 +6,8 @@ import {MapDispatchPropsType, MapStatePropsType} from "./UsersContainer";
 
 
 export class Users extends React.Component<MapStatePropsType & MapDispatchPropsType> {
-    constructor(props:MapStatePropsType & MapDispatchPropsType) {
-        super(props);
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
         })
